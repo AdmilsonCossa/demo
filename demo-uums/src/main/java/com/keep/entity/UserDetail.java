d4e883dc1e772d8ea3e075b949bcfa6efdb9f751
jeffreyzhang <shine8606@gmail.com>
2013-02-04 17:54:25 +0800

    uums

--- demo-uums/src/main/java/com/keep/entity/UserDetail.java
+++ demo-uums/src/main/java/com/keep/entity/UserDetail.java
@@ -1,24 +1,65 @@
 package com.keep.entity;
 
-import org.springframework.roo.addon.javabean.RooJavaBean;
-import org.springframework.roo.addon.tostring.RooToString;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+import com.keep.framework.entity.AbstractEntity;
 
 /**
  * @author 张朝峥
- *
+ * 
  */
-@RooJavaBean
-@RooToString
-public class UserDetail {
+@Entity
+@Table(name = "USER_DETAIL", uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })
+public class UserDetail extends AbstractEntity {
 
-	/**
-	 * 主键
-	 */
-	private Long id;
-	
 	/**
 	 * 名字
 	 */
-	private String name; 
-	
+	@Column(name="name")   
+	private String name;
+
+	/**
+	 * 性别
+	 */
+    @Column(name="sex")   
+	private Integer sex;
+
+	/**
+	 * 生日
+	 */
+	@Temporal(TemporalType.DATE)        
+    @Column(name="birth_date")        
+	private Date birthDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
 }
---
+++ demo-uums/src/main/java/com/keep/framework/entity/AbstractEntity.java
@@ -0,0 +1,61 @@
+package com.keep.framework.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * @author 张朝峥
+ *         <p>
+ *         抽象实体类。
+ *         </p>
+ */
+public class AbstractEntity {
+
+	/**
+	 * 主键
+	 */
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this, true);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, true);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.DEFAULT_STYLE);
+	}
+}
