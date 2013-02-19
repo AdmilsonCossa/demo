8fefdfddca00583935379e1737e27f97524fad30
jeffreyzhang <shine8606@gmail.com>
2013-02-19 22:50:59 +0800

    uums test

--- demo-uums/src/test/java/com/keep/framework/dao/GenericDaoTest.java
+++ demo-uums/src/test/java/com/keep/framework/dao/GenericDaoTest.java
@@ -1,5 +1,7 @@
 package com.keep.framework.dao;
 
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -8,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.keep.entity.LoginUser;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:META-INF/spring/applicationContext.xml")
@@ -18,6 +23,32 @@ public class GenericDaoTest extends AbstractJUnit4SpringContextTests {
 	
 	@Test
 	public void testGet() {
-		Assert.assertNotNull("d");
+		LoginUser u = new LoginUser();
+		u.setId(1L);
+		u.setAccount("zs");
+		u.setPassword("123");
+		
+		LoginUser u1 = new LoginUser();
+		u1.setId(2L);
+		u1.setAccount("zs1");
+		u1.setPassword("1234");
+		
+		LoginUser u2 = new LoginUser();
+		u2.setId(3L);
+		u2.setAccount("zs2");
+		u2.setPassword("12345");
+		genericDAO.save(u);
+		genericDAO.save(u1);
+		genericDAO.save(u2);
+		
+		List list = genericDAO.findAll(LoginUser.class);
+		Assert.assertEquals(list.size(), 3);
+		
+		LoginUser loginUser = genericDAO.get(LoginUser.class, 1L);
+		Assert.assertEquals(loginUser, u);
+		
+		genericDAO.delete(loginUser);
+		List list1 = genericDAO.findAll(LoginUser.class);
+		Assert.assertEquals(list1.size(), 2);
 	}
 }
